7624159305c3b563bf62fed1a6807be885e4a487
Md Imran Hossain <149870835+imranskr@users.noreply.github.com>
2023-11-25 21:30:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('.offer_dishe').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    dots:true,
+  });
+
+
+
+
+  $('.menu_card').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:true,
+    prevArrow:'<i class="fa-solid fa-caret-left prev_arrow"></i>',
+    nextArrow:'<i class="fa-solid fa-caret-right next_arrow"></i>',
+  });
